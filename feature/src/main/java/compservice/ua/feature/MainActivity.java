959fec199c4349ebce49f937973a6d71629e7e23
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-03-17 13:58:27 +0200

    Adding demo of using AutoCompleteTextView class

--- feature/src/main/java/compservice/ua/feature/MainActivity.java
+++ feature/src/main/java/compservice/ua/feature/MainActivity.java
@@ -2,12 +2,23 @@ package compservice.ua.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchText = (AutoCompleteTextView)findViewById(R.id.search_text);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, getResources().getStringArray(R.array.countries_array));
+
+        searchText.setAdapter(adapter);
+
+
     }
 }
